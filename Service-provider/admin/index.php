fcc1a799aae55089c03d60d21564aa36f4196e42
domaingamer <59157060+domaingamer@users.noreply.github.com>
2021-10-17 08:11:18 +0530

    Add files via upload

---
+++ Service-provider/admin/index.php
@@ -0,0 +1,35 @@
+<?php include('partials/menu.php');?>
+<!-- Main Content Start-->
+<div class="main-content">
+    <div class="wrappper">
+        <h1>DASHBOARD</h1>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <break>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <break>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <break>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <break>
+            Categories
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<!-- Main Content Ends-->
+
+<?php include('partials/footer.php');?>
